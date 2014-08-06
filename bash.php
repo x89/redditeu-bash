ce63592d3d97773743130030da883f1a276a7b0d
David Trail <napalm10@gmail.com>
2014-08-06 03:36:28 +0200

    More sex changes (no trannie)

--- bash.php
+++ bash.php
@@ -4,8 +4,8 @@ $MySQL['user'] = 'bash';
 $MySQL['pass'] = 'bash';
 $MySQL['db'] = 'bash';
 
-$Password = 'pisswurd';	
-$TheTitle = 'SCReddit QDB';
+$password = 'pisswurd';	
+$the_title = 'SCReddit QDB';
 $RequireCaptcha = 0;
 
 $Num['latest'] = 10;
@@ -18,9 +18,9 @@ mysql_connect('localhost', $MySQL['user'], $MySQL['pass']);
 mysql_select_db($MySQL['db']);
 
 function isAdmin(){
-	global $Password;
+	global $password;
 	if (isset($_COOKIE['bc_login'])){
-		if (base64_decode($_COOKIE['bc_login']) == $Password){
+		if (base64_decode($_COOKIE['bc_login']) == $password) { // Security 101
 			return TRUE;
 		} else {
 			return FALSE;
@@ -33,15 +33,15 @@ function isAdmin(){
 function smallMenu(){
 	$Menu = '<div class="menu"><a href="bash.php">home</a> / <a href="?latest">latest</a> / <a href="?browse">browse</a> / <a href="?random">random</a> / <a href="?add">add</a> / <a href="?top">top</a> / <a href="?search">search</a> / <a href="?chat">chat</a>';
 	if (isAdmin()){
-		$Menu.=' / <a href="?moderation">awaiting moderation</a> / <a href="?logout">logout</a>';
+		$Menu .= ' / <a href="?moderation">awaiting moderation</a> / <a href="?logout">logout</a>';
 	}
-	return $Menu.='</div>';
+	return $Menu .= '</div>';
 }
 
 function start_page($Title='', $Msg='') {
 
-	global $TheTitle;
-	if ($Title == '') $Title = $TheTitle;
+	global $the_title;
+	if ($Title == '') $Title = $the_title;
 	header('Content-type: text/html; charset=utf-8');	
 
 echo <<<__HEREDOC__
@@ -74,23 +74,23 @@ echo <<<__HEREDOC__
 	</script>
 </head>
 <body>
-<div class="topMenu">$Title</div>
+<div class="top-menu">$Title</div>
 __HEREDOC__;
 
 	echo smallMenu();
-	if ($Msg) { echo '<br><div class="welcome-box">'.$Msg.'</div>'; }
+	if ($Msg) echo '<br><div class="welcome-box">'.$Msg.'</div>';
 
 }
 
-function show_quote($Quote,$ShowTime=FALSE){
+function show_quote($Quote, $ShowTime = FALSE) {
 
 	if (isAdmin()){
-		$del='<a href="#" onclick="javascript:ConfirmChoice(\'?del='.$Quote['id'].'\')">[x]</a>';
-		if ($Quote['active']==0){
-			$approve='<a href="?moderation=1&approve='.$Quote['id'].'">[a]</a>';
+		$del = '<a href="#" onclick="javascript:ConfirmChoice(\'?del='.$Quote['id'].'\')">[x]</a>';
+		if ($Quote['active'] == 0){
+			$approve = '<a href="?moderation=1&approve='.$Quote['id'].'">[a]</a>';
 		}
 	} else {
-		$del=$approve='';
+		$del = $approve = '';
 	}
 
 	if ($Quote){
@@ -100,65 +100,65 @@ function show_quote($Quote,$ShowTime=FALSE){
 	}
 }
 
-if (isset($_GET['pass'])){
+if (isset($_GET['pass'])) {
 
-	if ($_GET['pass']==$Password){
-		setcookie('bc_login',base64_encode($_GET['pass']),time()+60*60*24*90); // 90 days
-		start_page($TheTitle.' - Logged In','Vilkommen administrator, you are now logged in and can browse to delete quotes.<br><br>');
+	if ($_GET['pass'] == $password) {
+		setcookie('bc_login', base64_encode($_GET['pass']), time() + 60 * 60 * 24 * 90); // 90 days
+		start_page($the_title.' - Logged In','Vilkommen administrator, you are now logged in and can browse to delete quotes.<br><br>');
 	}
 
-} elseif (isset($_GET['chat'])){
-	start_page($TheTitle.' - Chitty Chatty','<iframe src="http://kiwiirc.com/client/irc.quakenet.org/redditeu/?nick=webber|?" style="border:0; width:100%; height:450px;"></iframe><br /><strong>irc.quakenet.org #redditeu</strong><br><br>');
+} elseif (isset($_GET['chat'])) {
+	start_page($the_title.' - Chitty Chatty','<iframe src="http://kiwiirc.com/client/irc.quakenet.org/redditeu/?nick=webber|?" style="border:0; width:100%; height:450px;"></iframe><br /><strong>irc.quakenet.org #redditeu</strong><br><br>');
 
-} elseif (isset($_GET['logout'])){
+} elseif (isset($_GET['logout'])) {
 
-	setcookie('bc_login','',time()-3600);
-	start_page($TheTitle.' - Logged out','You have been logged out. Cheerio!');
+	setcookie('bc_login', '', time() - 3600);
+	start_page($the_title.' - Logged out','You have been logged out. Cheerio!');
 	echo '</body></html>';
 
-} elseif (isset($_GET['del'])){
+} elseif (isset($_GET['del'])) {
 
 	if (isAdmin()){
-		$quoteId=mysql_real_escape_string(intval($_GET['del']));
+		$quoteId = mysql_real_escape_string(intval($_GET['del']));
 		mysql_query("DELETE FROM bc_quotes WHERE id = $quoteId LIMIT 1");
 		mysql_query("DELETE FROM bc_votes WHERE quote_id = $quoteId LIMIT 1");
 	}
-	start_page($TheTitle.' - Quote Deleted','Quote deleted squire.');
+	start_page($the_title.' - Quote Deleted','Quote deleted squire.');
 
-} elseif (isset($_GET['del_all'])){
+} elseif (isset($_GET['del_all'])) {
 
 	if (isAdmin()){
 		mysql_query("DELETE FROM bc_quotes WHERE active = 0");
 	}
-	start_page($TheTitle.' - Quotes Deleted','Quote queue deleted squire.');
+	start_page($the_title.' - Quotes Deleted','Quote queue deleted squire.');
 
-} elseif (isset($_GET['browse'])){
+} elseif (isset($_GET['browse'])) {
 
-	start_page($TheTitle.' - Browse');
+	start_page($the_title.' - Browse');
 
-	$page = intval(mysql_real_escape_string($_GET['browse']))-1;
+	$page = intval(mysql_real_escape_string($_GET['browse'])) - 1;
 	if ($page < 1) {
-		$page=0;
+		$page = 0;
 	}
 	$NumPP = $Num['browsePP'];
 
-	$getTotal = mysql_query("SELECT COUNT(*) AS count FROM bc_quotes WHERE active = 1");
-	$Total = mysql_result($getTotal,0,0);
+	$sql = mysql_query("SELECT COUNT(*) AS count FROM bc_quotes WHERE active = 1");
+	$total = mysql_result($sql, 0, 0);
 
-	for ($x = 0; $x < intval($Total/$NumPP)+1; $x++) {
-		$pagesString.='<a href="?browse='.($x+1).'">'.($x+1).'</a> - ';
+	for ($x = 0; $x < intval($total/$NumPP)+1; $x++) {
+		$pagesString .= '<a href="?browse='.($x + 1).'">'.($x+1).'</a> - ';
 	}
 
 	echo '<br>'.substr($pagesString,0,-3).'<br><br/>';
 
-	$Start = $page*$NumPP;
-	$End = $page+$NumPP;
+	$start = $page * $NumPP;
+	$end = $page + $NumPP;
 
-	$getResults = mysql_query("SELECT * FROM bc_quotes WHERE active = 1 ORDER BY id ASC LIMIT $Start, $End");
+	$sql = mysql_query("SELECT * FROM bc_quotes WHERE active = 1 ORDER BY id ASC LIMIT $start, $end");
 
-	if (@mysql_num_rows($getResults)>0){
-		while ($Result = mysql_fetch_assoc($getResults)){
-			show_quote($Result);
+	if (@mysql_num_rows($sql) > 0){
+		while ($row = mysql_fetch_assoc($sql)){
+			show_quote($row);
 		}
 	} else {
 		echo 'No results found homeslice.';
@@ -166,9 +166,9 @@ if (isset($_GET['pass'])){
 
 } elseif (isset($_GET['search'])){
 
-	start_page($TheTitle.' - Search');
+	start_page($the_title.' - Search');
 
-	if (strlen($_GET['search'])>0){
+	if (strlen($_GET['search']) > 0){
 		$searchQuery = mysql_real_escape_string($_GET['search']);
 	}
 
@@ -178,11 +178,11 @@ if (isset($_GET['pass'])){
 		</form>';
 
 if (strlen($_GET['search'])>0){
-	$getSearch = mysql_query("SELECT * FROM bc_quotes WHERE active = 1 AND quote LIKE '%$searchQuery%' LIMIT {$Num['search']}");
+	$sql = mysql_query("SELECT * FROM bc_quotes WHERE active = 1 AND quote LIKE '%$searchQuery%' LIMIT {$Num['search']}");
 
-	if (@mysql_num_rows($getSearch) > 0){
-		while ($Search = mysql_fetch_assoc($getSearch)){
-			show_quote($Search);
+	if (@mysql_num_rows($sql) > 0){
+		while ($row = mysql_fetch_assoc($sql)){
+			show_quote($row);
 		}
 
 		echo '<form method="GET" action="'.$_SERVER['REQUEST_URI'].'">
@@ -198,57 +198,57 @@ if (strlen($_GET['search'])>0){
 } elseif (isset($_GET['add'])){
 
 	if ($RequireCaptcha){
-		require_once('./recaptchalib.php');
+		require_once('./recaptchalib.php'); // We don't actually have this file.
 	}
 
 	if (isset($_POST['submit'])){
 
 		if ($RequireCaptcha){
 			$privatekey = "6Lc8Q8ESAAAAADAgiufKhG7J8vlTJnXMsHrAtOww";
-			$resp = recaptcha_check_answer ($privatekey,$_SERVER["REMOTE_ADDR"],$_POST["recaptcha_challenge_field"],$_POST["recaptcha_response_field"]);
+			$resp = recaptcha_check_answer ($privatekey, $_SERVER["REMOTE_ADDR"], $_POST["recaptcha_challenge_field"], $_POST["recaptcha_response_field"]);
 
 			if (!$resp->is_valid) {
-				$Msg="<strong>The reCAPTCHA wasn't entered correctly. Try it again.</strong><br>";
+				$Msg = "<strong>The reCAPTCHA wasn't entered correctly. Try it again.</strong><br>";
 			}
 		}
 
 		if (!$Msg){
-			$timestamp=time();
-			$ip=$_SERVER['REMOTE_ADDR'];
-			$quote=mysql_real_escape_string(substr($_POST['quote'],0,9993000));
+			$timestamp = time();
+			$ip = $_SERVER['REMOTE_ADDR'];
+			$quote = mysql_real_escape_string(substr($_POST['quote'], 0, 9993000));
 
-			if ($_POST['strip']=='on'){
-				$quoteSplode=explode('\n',$quote);
-				foreach($quoteSplode as $Line => $Value){
-					$temp=strpos($Value,'<');
+			if ($_POST['strip'] == 'on'){
+				$quoteSplode = explode('\n', $quote);
+				foreach ($quoteSplode as $Line => $Value){
+					$temp = strpos($Value, '<');
 
-					if ($temp===FALSE){
-						$quoteStripped.=$Value;
+					if ($temp === FALSE){
+						$quoteStripped .= $Value;
 					} else {
-						$quoteStripped.=substr($Value,$temp);
+						$quoteStripped .= substr($Value, $temp);
 					}
 				}
 				$quoteStripped = str_replace(' ', ' ', $quoteStripped);
-				$quote=$quoteStripped;
+				$quote = $quoteStripped;
 			}
 			if (strlen($quote)>5){
 				mysql_query("INSERT INTO bc_quotes (timestamp, ip, quote, active) VALUES('$timestamp','$ip','$quote',0)");
-				$lastId=mysql_insert_id();
-				$Msg='<strong>Quote added as <a href="?'.$lastId.'">#'.$lastId.'</a>. Thanks for participating :-)</strong><br />';
+				$lastId = mysql_insert_id();
+				$Msg = '<strong>Quote added as <a href="?'.$lastId.'">#'.$lastId.'</a>. Thanks for participating :-)</strong><br />';
 			} else {
-				$Msg='Quote not added.';
+				$Msg = 'Quote not added.';
 			}
 		}
 	}
 
-	start_page($TheTitle.' - Add',$Msg);
+	start_page($the_title.' - Add',$Msg);
 
 	echo '<br><form style="background-color:#CCC;margin:10px;padding:10px;border:1px solid #333;" method="POST" action="'.$_SERVER['REQUEST_URI'].'">
 		<textarea name="quote" maxlength="3000" style="width:700px;height:300px;">'.stripslashes($_POST['quote']).'</textarea><br>
 		Attempt to strip timestamps? <input type="checkbox" name="strip" checked="checked"><br /><br />';
 
 	if ($RequireCaptcha){
-		$publickey = "6Lc8Q8ESAAAAAB8ZgZFKotSQ5dJQ7-IWVoYeTKlE"; // you got this from the signup page
+		$publickey = "6Lc8Q8ESAAAAAB8ZgZFKotSQ5dJQ7-IWVoYeTKlE";
 		echo recaptcha_get_html($publickey);
 	}
 
@@ -258,14 +258,14 @@ if (strlen($_GET['search'])>0){
 } elseif (isset($_GET['approve'])){
 
 	if (isAdmin()){
-		$quoteId=intval(mysql_real_escape_string($_GET['approve']));
+		$quoteId = intval(mysql_real_escape_string($_GET['approve']));
 		mysql_query("UPDATE bc_quotes SET active = 1 WHERE id = $quoteId");
-		start_page($TheTitle,'Quote approved and is now live on the site.');
+		start_page($the_title,'Quote approved and is now live on the site.');
 	}
 
 } elseif (isset($_GET['random'])){
 
-	start_page($TheTitle.' - Random Quotes','Refresh page to view more random quotes.');
+	start_page($the_title.' - Random Quotes','Refresh page to view more random quotes.');
 
 	$getRand=mysql_query("SELECT * FROM bc_quotes WHERE active = 1 ORDER BY RAND() LIMIT {$Num['random']}");
 
@@ -277,25 +277,23 @@ if (strlen($_GET['search'])>0){
 
 } elseif (isset($_GET['top'])){
 
-	start_page($TheTitle.' - Top Rated Quotes');
+	start_page($the_title.' - Top Rated Quotes');
 
-	$getTop=mysql_query("SELECT * FROM bc_quotes WHERE active = 1 ORDER BY popularity DESC LIMIT {$Num['top']}")or die(mysql_error());
+	$sql = mysql_query("SELECT * FROM bc_quotes WHERE active = 1 ORDER BY popularity DESC LIMIT {$Num['top']}") or die(mysql_error());
 
-	if (mysql_num_rows($getTop)>0){
-		while ($Top=mysql_fetch_assoc($getTop)){
-			show_quote($Top);
+	if (mysql_num_rows($sql) > 0){
+		while ($row = mysql_fetch_assoc($sql)){
+			show_quote($row);
 		}
 	}
 
 } elseif (isset($_GET['latest'])){
 
-	start_page($TheTitle.' - Latest Quotes');
-
-	$getLatest=mysql_query("SELECT * FROM bc_quotes WHERE active = 1 ORDER BY id DESC LIMIT {$Num['latest']}")or die(mysql_error());
-
-	if (mysql_num_rows($getLatest)>0){
-		while ($Latest=mysql_fetch_assoc($getLatest)){
-			show_quote($Latest);
+	start_page($the_title.' - Latest Quotes');
+	$sql = mysql_query("SELECT * FROM bc_quotes WHERE active = 1 ORDER BY id DESC LIMIT {$Num['latest']}") or die(mysql_error());
+	if (mysql_num_rows($sql)>0){
+		while ($row = mysql_fetch_assoc($sql)){
+			show_quote($row);
 		}
 	}
 
@@ -314,13 +312,13 @@ if (strlen($_GET['search'])>0){
 
 } elseif (isset($_GET['moderation'])) {
 
-	start_page($TheTitle.' - Quotes in Moderation');
+	start_page($the_title.' - Quotes in Moderation');
 
 	if (isAdmin()){
-		$getModeration=mysql_query("SELECT * FROM bc_quotes WHERE active = 0 ORDER BY id ASC LIMIT 100");
+		$getModeration = mysql_query("SELECT * FROM bc_quotes WHERE active = 0 ORDER BY id ASC LIMIT 100");
 
 		if (mysql_num_rows($getModeration)>0){
-			while ($Moderation=mysql_fetch_assoc($getModeration)){
+			while ($Moderation = mysql_fetch_assoc($getModeration)){
 				show_quote($Moderation);
 			}
 		} else {
@@ -329,9 +327,8 @@ if (strlen($_GET['search'])>0){
 	}
 
 } elseif (empty($_GET)){
-	// Display Homepage
 
-	start_page($TheTitle.' - Home','<a href="http://www.screddit.eu/chat/">irc.quakenet.org #redditeu</a>');
+	start_page($the_title.' - Home','<a href="http://www.screddit.eu/chat/">irc.quakenet.org #redditeu</a>');
 
 	$sql = mysql_query("SELECT * FROM bc_quotes WHERE active = 1 ORDER BY RAND() LIMIT 5");
 	$result = mysql_fetch_assoc($sql);
@@ -343,51 +340,45 @@ if (strlen($_GET['search'])>0){
 	}
 
 } else {
-	// Display a Single Quote
 
 	if (isset($_SERVER['QUERY_STRING'])){
 
-		$array = array(0 => '&v=rox',1 => '&v=sux');
-		$quoteId = mysql_real_escape_string(str_replace($array,'',$_SERVER['QUERY_STRING']));
+		$array = array(0 => '&v=rox', 1 => '&v=sux');
+		$quoteId = mysql_real_escape_string(str_replace($array, '', $_SERVER['QUERY_STRING']));
+
+		if ($_GET['v'] == 'sux' or $_GET['v'] == 'rox') {
+			$voteCheck = mysql_query("SELECT * FROM bc_votes WHERE quote_id = $quoteId AND ip = '$ip'");
+			$ip = $_SERVER['REMOTE_ADDR'];
+		}
 
 		if ($_GET['v'] == 'sux'){
 
-			$ip=$_SERVER['REMOTE_ADDR'];
-			$voteCheck=mysql_query("SELECT * FROM bc_votes WHERE quote_id = $quoteId AND ip = '$ip'");
-
-			// Only count vote if not already voted, but display thank you regardless
-			if (mysql_num_rows($voteCheck)<1){
-				mysql_query("INSERT INTO bc_votes (quote_id,ip,type) VALUES('$quoteId','$ip',1)");
+			if (mysql_num_rows($voteCheck) < 1){
+				mysql_query("INSERT INTO bc_votes (quote_id,ip,type) VALUES('$quoteId','$ip',1)") or die(mysql_error());
 				mysql_query("UPDATE bc_quotes SET popularity = popularity - 1 WHERE id = $quoteId") or die(mysql_error());
 			}
-
-			start_page($TheTitle.' - Voted','Thanks for your vote.<br><br>');
+			start_page($the_title.' - Voted','Thanks for your vote.<br><br>');
 
 		} elseif ($_GET['v'] == 'rox'){
 
-			$ip=$_SERVER['REMOTE_ADDR'];
-			$voteCheck=mysql_query("SELECT * FROM bc_votes WHERE quote_id = $quoteId AND ip = '$ip'");
-
-			// Only count vote if not already voted, but display thank you regardless
-			if (mysql_num_rows($voteCheck)<1){
+			if (mysql_num_rows($voteCheck) < 1){
 				mysql_query("INSERT INTO bc_votes (quote_id,ip,type) VALUES('$quoteId','$ip',2)") or die(mysql_error());
 				mysql_query("UPDATE bc_quotes SET popularity = popularity + 1 WHERE id = $quoteId") or die(mysql_error());
 			}
-
-			start_page($TheTitle.' - Voted','Thank you for your vote!<br><br>');
+			start_page($the_title.' - Voted','Thank you for your vote!<br><br>');
 
 		} else {
 
-			$getQuote = mysql_query("SELECT * FROM bc_quotes WHERE id = '$quoteId'");
-			$Quote = mysql_fetch_assoc($getQuote);
+			$sql = mysql_query("SELECT * FROM bc_quotes WHERE id = '$quoteId'");
+			$Quote = mysql_fetch_assoc($sql);
 
 			if (empty($Quote) || !isset($Quote)){
-				start_page($TheTitle);
+				start_page($the_title);
 			} elseif ($Quote['active'] == 0){
-				start_page($TheTitle.' - Quote Pending Approval','Quote Pending Approval');
+				start_page($the_title.' - Quote Pending Approval','Quote Pending Approval');
 			} elseif ($Quote['active'] == 1){
 				$ReplaceMe = array(0 => '\n', 1 => '\r', 2 => '\t');
-				start_page($TheTitle.' - '.substr(str_replace($ReplaceMe,' ',htmlspecialchars($Quote['quote'])), 0, 80));
+				start_page($the_title.' - '.substr(str_replace($ReplaceMe,' ',htmlspecialchars($Quote['quote'])), 0, 80));
 				show_quote($Quote, 1);
 			}
 		}
@@ -397,21 +388,21 @@ if (strlen($_GET['search'])>0){
 
 if (!isset($_GET['json'])) {
 
-	$countQuotes = mysql_query("SELECT COUNT(*) AS count FROM bc_quotes GROUP BY active ORDER BY active DESC");
+	$sql = mysql_query("SELECT COUNT(*) AS count FROM bc_quotes GROUP BY active ORDER BY active DESC");
 
-	$Approved = @mysql_result($countQuotes, 0, 0);
+	$Approved = @mysql_result($sql, 0, 0);
 	if ($Approved < 1){
-		$Approved=0;
+		$Approved = 0;
 	}
 
-	$Pending = @mysql_result($countQuotes, 1, 0);
+	$Pending = @mysql_result($sql, 1, 0);
 	if ($Pending < 1){
 		$Pending = 0;
 	}
 	echo smallMenu();
 echo <<<__HEREDOC__
-<div class="topMenu">
-	<div class="left-element">
+<div class="bottom-menu">
+	<div>
 		$Approved quotes approved; $Pending quotes pending
 	</div>
 </div>
