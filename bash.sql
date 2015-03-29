de9b48c44af3387f0d3255a6a6ef48ec7e562ad9
Andreas Lutro <anlutro@gmail.com>
2015-03-29 15:04:54 +0200

    make quote ip optional

--- bash.sql
+++ bash.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "bc_quotes" (
 	"id" serial PRIMARY KEY,
 	"timestamp" integer NOT NULL,
-	"ip" varchar(255) NOT NULL,
+	"ip" varchar(255),
 	"quote" text NOT NULL,
 	"active" boolean NOT NULL,
 	"popularity" integer NOT NULL
